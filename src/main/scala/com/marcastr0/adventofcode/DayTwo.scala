98914015fe8ca05d1916ab7e61ae5e72184186ad
Mario Castro Squella <mario.castro.squella@gmail.com>
2017-12-02 16:26:08 -0300

    Functions and tests for Day Two, part one.

--- src/main/scala/com/marcastr0/adventofcode/DayTwo.scala
+++ src/main/scala/com/marcastr0/adventofcode/DayTwo.scala
@@ -1,5 +1,18 @@
 package com.marcastr0.adventofcode
 
 object DayTwo {
+  def stringToSpreadsheet(spreadsheet: String): List[List[Int]] = {
+    spreadsheet.split("\n").toList map {
+      row => row.split("\t").toList.map(_.toInt)
+    }
+  }
 
+  def getRowDifference(row: List[Int]): Int = {
+    row.sorted.reverse.head - row.min
+  }
+
+  def partOne(spreadsheet: String): Int = {
+    val xs = stringToSpreadsheet(spreadsheet) map (row => getRowDifference(row))
+    xs.sum
+  }
 }
--- src/test/scala/com/marcastr0/adventofcode/DayTwoSpec.scala
+++ src/test/scala/com/marcastr0/adventofcode/DayTwoSpec.scala
@@ -3,5 +3,26 @@ package com.marcastr0.adventofcode
 import org.scalatest.FlatSpec
 
 class DayTwoSpec extends FlatSpec {
+  "stringToSpreadsheet" should "convert a string with the spreadsheet to a list o lists with integers" in {
+    val spreadsheet =
+      """5	1	9	5
+        |7	5	3
+        |2	4	6	8""".stripMargin
+    assert(DayTwo.stringToSpreadsheet(spreadsheet) == List(List(5, 1, 9, 5), List(7, 5, 3), List(2, 4, 6, 8)))
+  }
+
+  "getRowDifference" should "determine the difference between the largest value and the smallest value in the row" in {
+    assert(DayTwo.getRowDifference(List(5, 1, 9, 5)) == 8)
+    assert(DayTwo.getRowDifference(List(7, 5, 3)) == 4)
+    assert(DayTwo.getRowDifference(List(2, 4, 6, 8)) == 6)
+  }
+
+  "partOne" should "calculate the spreadsheet's checksum" in {
+    val spreadsheet =
+      """5	1	9	5
+        |7	5	3
+        |2	4	6	8""".stripMargin
+    assert(DayTwo.partOne(spreadsheet) == 18)
+  }
 
 }
