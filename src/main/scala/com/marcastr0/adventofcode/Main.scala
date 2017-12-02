2ab65ddae5cc2e0e284d0b4a6382b051b7ab3493
Mario Castro Squella <mario.castro.squella@gmail.com>
2017-12-02 16:44:15 -0300

    Solution for Day Two, part one.

--- src/main/scala/com/marcastr0/adventofcode/Main.scala
+++ src/main/scala/com/marcastr0/adventofcode/Main.scala
@@ -28,7 +28,14 @@ object Main extends App {
    * Solution to Day 1: http://adventofcode.com/2017/day/1
    */
   val dayOneInput = Source.fromResource("DayOne.txt").getLines.mkString
-  println("+++ Day One +++")
+  println("--- Day 1: Inverse Captcha ---")
   println("The solution to the captcha is " + DayOne.partOne(dayOneInput).toString)
   println("The solution to the new captcha is " + DayOne.partTwo(dayOneInput).toString)
+
+  /*
+   * Solution to Day 2: http://adventofcode.com/2017/day/2
+   */
+  val dayTwoInput = Source.fromResource("DayTwo.txt").getLines.mkString("\n")
+  println("--- Day 2: Corruption Checksum ---")
+  println("The checksum for the spreadsheet is " + DayTwo.partOne(dayTwoInput))
 }
